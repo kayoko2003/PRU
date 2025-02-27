ab0c53c5c450096a67178bebe17bc6e6f302bd7d
Trịnh Đình Ngọc <ngocqd2@gmail.com>
2025-02-27 17:20:35 +0700

    new commit
    new commit

---
+++ Assets/Scripts/Enemies/Enemy Movement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    public float moveSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+    }
+}
